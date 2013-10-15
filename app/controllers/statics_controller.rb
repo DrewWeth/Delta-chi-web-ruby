8a6cfa35bcbedfc49836a01a4360a949254d1599
DrewWeth <dgwetherington@gmail.com>
2013-10-14 22:26:07 -0500

    static links work

---
+++ app/controllers/statics_controller.rb
@@ -0,0 +1,9 @@
+class StaticsController < ApplicationController
+	def home
+
+	end
+
+	def about
+
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DeltaChiWebsite::Application.config.secret_key_base = '4e528639723fb3bfd039ab73f3495af95b6616254630df1832e9af62dc7aafd88179d1db59730d97fe0ed5257b89e445780d91468ac831f051b6e4d4aa2fe49d'
