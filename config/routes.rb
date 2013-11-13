586a74b94a0c4b894b127c368912e163f7b30189
DrewWeth <dgwetherington@gmail.com>
2013-11-12 18:17:29 -0600

    added a lot and its not broken

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,8 @@ DeltaChiWebsite::Application.routes.draw do
   get "recruitment" => "statics#recruitment"
   get "involvement" => "statics#involvement"
   get "donations" => "statics#donations"
-  get "meet-us" => "statics#meet-us"
+  get "meet" => "statics#meet"
+  get "contact" => "statics#contact"
   get "login" => "statics#login"
  
   # The priority is based upon order of creation: first created -> highest priority.
